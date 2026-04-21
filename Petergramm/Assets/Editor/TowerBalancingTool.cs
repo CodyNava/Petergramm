7252c6460e71a7846e7e668675a69b18a61e208d
Jan-Lukas Dehne <kingdomeheart16@gmail.com>
2026-04-21 16:19:00 +0200

    [+] Tower Balancing Tool Set-Up

---
+++ Petergramm/Assets/Editor/TowerBalancingTool.cs
@@ -0,0 +1,59 @@
+using _01_Scripts._01_Tower.Data;
+using UnityEditor;
+
+namespace Editor
+{
+    public class TowerBalancingTool : EditorWindow
+    {
+        public TowerBaseSO towerPrefab;
+        public string towerName;
+        public float hitPoints;
+        public float damage;
+        public float range;
+        public float attacksPerSecond;
+
+
+        [MenuItem("Tools/Tower Balancing")]
+        public static void ShowWindow()
+        {
+            GetWindow(typeof(TowerBalancingTool));
+        }
+
+        private void OnGUI()
+        {
+            towerPrefab = EditorGUILayout.ObjectField("Tower", towerPrefab, typeof(TowerBaseSO), false) as TowerBaseSO;
+            if (towerPrefab != null)
+            {
+                towerName = towerPrefab.towerName;
+                hitPoints = towerPrefab.baseStats.maxHp;
+                damage = towerPrefab.baseStats.damage;
+                range = towerPrefab.baseStats.range;
+                attacksPerSecond = towerPrefab.baseStats.attacksPerSecond;
+
+                EditorGUILayout.Space();
+                EditorGUI.indentLevel = 1;
+              
+                
+                towerName = EditorGUILayout.TextField("Name", towerName);
+                hitPoints = EditorGUILayout.FloatField("HitPoints", hitPoints);
+                damage = EditorGUILayout.FloatField("Damage", damage);
+                range = EditorGUILayout.FloatField("Range", range);
+                attacksPerSecond = EditorGUILayout.Slider("Attacks per Second", attacksPerSecond, 0f, 5f);
+                
+                EditorGUILayout.Space();
+                EditorGUILayout.FloatField("DPS", damage * attacksPerSecond);
+
+                UpdateValues();
+            }
+        }
+
+        private void UpdateValues()
+        {
+            towerPrefab.towerName = towerName;
+            towerPrefab.baseStats.maxHp = hitPoints;
+            towerPrefab.baseStats.damage = damage;
+            towerPrefab.baseStats.range = range;
+            towerPrefab.baseStats.attacksPerSecond = attacksPerSecond;
+        }
+    }
+}
