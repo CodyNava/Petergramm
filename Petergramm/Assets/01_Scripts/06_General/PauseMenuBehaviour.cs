d589c4753033c2fb9a170d733806f0be859f4ec2
Jan-Lukas Dehne <kingdomeheart16@gmail.com>
2026-08-10 15:34:17 +0200

    [+] Pause Menu

---
+++ Petergramm/Assets/01_Scripts/06_General/PauseMenuBehaviour.cs
@@ -0,0 +1,59 @@
+using _01_Scripts._05_InputSystem;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _01_Scripts._06_General
+{
+    public class PauseMenuBehaviour : MonoBehaviour
+    {
+        [SerializeField] private GameObject pauseMenu;
+        private bool _paused;
+
+        private void Awake()
+        {
+            _paused = false;
+            pauseMenu.SetActive(false);
+            
+        }
+        
+        private void Update()
+        {
+            if (InputManager.Input.Camera.Pause.WasPressedThisFrame())
+            {
+                if (_paused)
+                {
+                    Resume();
+                }
+                else
+                {
+                    Pause();
+                }
+            }
+        }
+
+        public void Resume()
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+            _paused = false;
+        }
+
+        private void Pause()
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+            _paused = true;
+        }
+
+        public void SaveGame()
+        {
+            
+        }
+
+        public void BackToMenu(string sceneName)
+        {
+            Resume();
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
